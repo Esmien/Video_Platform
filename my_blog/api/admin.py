4efd0839dbfc90e54d7c73be69aafc02f4e24f6e
Владислав Сороковых <black.walf16@gmail.com>
2026-04-08 00:54:24 +0300

    feat: немного настроил админку

--- my_blog/api/admin.py
+++ my_blog/api/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
-
 from .models import Video, VideoFile, Like
-# Register your models here.
-admin.site.register(Video)
-admin.site.register(VideoFile)
-admin.site.register(Like)
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'is_published', 'total_likes', 'created_at')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('name', 'owner__username')
+    readonly_fields = ('total_likes', 'created_at')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('owner')
+
+
+@admin.register(VideoFile)
+class VideoFileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video', 'quality', 'file')
+    list_filter = ('quality',)
+    search_fields = ('video__name',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('video')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video', 'user')
+    search_fields = ('video__name', 'user__username')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('video', 'user')
