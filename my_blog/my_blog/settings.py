ef2c57a92c54e395b6629e436bcef6836f149338
Владислав Сороковых <black.walf16@gmail.com>
2026-04-08 00:57:44 +0300

    feat: добавил заготовки под авторизацию

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Указываем spectacular как дефолтный генератор OpenAPI схемы
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
