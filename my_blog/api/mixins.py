0542aff1f230dd29d6fa652b450ff0aa57e399b1
Владислав Сороковых <black.walf16@gmail.com>
2026-04-10 00:02:20 +0300

    refactor: вынес бизнес-логику в сервисный слой

---
+++ my_blog/api/mixins.py
@@ -0,0 +1,15 @@
+from django.db.models import QuerySet
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+
+class PaginatedResponseMixin:
+    def _get_paginated_response(self: GenericAPIView, queryset: QuerySet) -> Response:
+        """Вспомогательный метод для пагинации кастомных QuerySet"""
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            return self.get_paginated_response(page)
+
+        return Response(list(queryset), status=status.HTTP_200_OK)
