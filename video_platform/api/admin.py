703ce2a6018022160404da7df3524e0276af0990
Владислав Сороковых <black.walf16@gmail.com>
2026-04-15 00:04:07 +0300

    fix: исправил зависания при выборе полей в админке
    В прошлом варианте, при попытке выбрать запись из выпадающего списка при большом количестве записей, страница зависала. Теперь данные подгружаются порционно

--- video_platform/api/admin.py
+++ video_platform/api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from .mixins import PaginatedResponseMixin, CursorPaginationMixin
 from .models import Video, VideoFile, Like
 
 @admin.register(Video)
@@ -7,6 +9,8 @@ class VideoAdmin(admin.ModelAdmin):
     list_filter = ('is_published', 'created_at')
     search_fields = ('name', 'owner__username')
     readonly_fields = ('total_likes', 'created_at')
+
+    autocomplete_fields = ('owner',)
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('owner')
@@ -17,6 +21,7 @@ class VideoFileAdmin(admin.ModelAdmin):
     list_display = ('id', 'video', 'quality', 'file')
     list_filter = ('quality',)
     search_fields = ('video__name',)
+    autocomplete_fields = ('video',)
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('video')
@@ -26,6 +31,8 @@ class VideoFileAdmin(admin.ModelAdmin):
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('id', 'video', 'user')
     search_fields = ('video__name', 'user__username')
+
+    autocomplete_fields = ('video', 'user')
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('video', 'user')
